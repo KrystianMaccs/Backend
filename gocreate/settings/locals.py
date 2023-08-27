0f39434d10cb51772e285abb34dfbb62238a02b9
olaifa ibukun <ibukunolaifa1984@example.com>
2023-08-27 06:18:57 +0100

    updated db

--- gocreate/settings/locals.py
+++ gocreate/settings/locals.py
@@ -96,6 +96,11 @@ DATABASES = {
     }
 }
 
+# DATABASES["default"] = dj_database_url.parse(
+#     os.getenv("PROD_DATABASE_URL"),
+#     conn_max_age=600,
+# )
+
 INTERNAL_IPS = [
 "127.0.0.1",
 ]
